295e5cff0ed24616f8b998bae14ececf93304ff4
ShantanuPimpale <shantanunitinpimpale@gmail.com>
2024-04-05 00:28:40 +0530

    New Changes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './pages/Home/home';
+import Login from './pages/Login/login';
+import Signup from './pages/Signup/signup';
+
+function App() {
+  return (
+    <div className='p-4 h-screen flex justify-center items-center'>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
