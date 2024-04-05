abd4855e010542e8b8f2dc42428c5fb1b5d66590
ShantanuPimpale <shantanunitinpimpale@gmail.com>
2024-04-06 00:14:11 +0530

    New Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
+import {Routes,Route} from "react-router-dom"
 import Home from './pages/Home/home';
 import Login from './pages/Login/login';
 import Signup from './pages/Signup/signup';
+import {Toaster} from "react-hot-toast"
 
 function App() {
   return (
     <div className='p-4 h-screen flex justify-center items-center'>
-      <Home />
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<Signup/>} />
+    </Routes>
+    <Toaster/>
     </div>
   );
 }
